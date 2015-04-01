f91a5f5f05145bdcb75b8bc0915f17b6c6e7a3a8
jbanety <jbanety@gmail.com>
2015-04-01 14:07:47 +0200

    Ajout DI

--- src/HtmlView.php
+++ src/HtmlView.php
@@ -9,97 +9,15 @@
 
 namespace EtdSolutions\View;
 
-use EtdSolutions\Model\Model;
-use Joomla\Renderer\RendererInterface;
+use Joomla\DI\ContainerAwareInterface;
+use Joomla\DI\ContainerAwareTrait;
 use Joomla\View\BaseHtmlView;
-
-
 
 /**
  * Vue HTML
  */
-class HtmlView extends BaseHtmlView {
+class HtmlView extends BaseHtmlView implements ContainerAwareInterface {
 
-    /**
-     * @var $name string Le nom de la vue.
-     */
-    protected $name;
+    use ContainerAwareTrait;
 
-    /**
-     * @var $defaultModel string Le nom du model par défaut.
-     */
-    protected $defaultModel;
-
-    /**
-     * Method to instantiate the view.
-     *
-     * @param   Model              $model     The model object.
-     * @param   RendererInterface  $renderer  The renderer object.
-     *
-     * @since   __DEPLOY_VERSION__
-     */
-    public function __construct(Model $model = null, RendererInterface $renderer = null) {
-
-        $this->defaultModel = $this->getName();
-
-        $model    = isset($model) ? $model : $this->getModel();
-
-        parent::__construct($model, $renderer);
-    }
-
-    /**
-     * Méthode pour charger un model.
-     *
-     * @param string $name           Le nom du modèle. Facultatif.
-     * @param   bool $ignore_request Utilisé pour ignorer la mise à jour de l'état depuis l'input.
-     *
-     * @return Model Le modèle.
-     *
-     * @throws \RuntimeException
-     */
-    protected function getModel($name = null, $ignore_request = false) {
-
-        if (!isset($name)) {
-            if (isset($this->defaultModel)) {
-                $name = $this->defaultModel;
-            } else {
-                throw new \RuntimeException("Unable to find a model", 500);
-            }
-        }
-
-        $name = ucfirst($name);
-
-        return Model::getInstance($name, $ignore_request);
-
-    }
-
-    /**
-     * Méthode pour récupérer le nom de la vue.
-     *
-     * @return  string  Le nom de la vue.
-     *
-     * @throws  \RuntimeException
-     */
-    public function getName() {
-
-        if (empty($this->name)) {
-            $r         = null;
-            $classname = join('', array_slice(explode('\\', get_class($this)), -1));
-            if (!preg_match('/(.*)View/i', $classname, $r)) {
-                throw new \RuntimeException('Unable to detect view name', 500);
-            }
-            $this->name = $r[1];
-        }
-
-        return $this->name;
-    }
-
-    /**
-     * Méthode appelée avant la création du rendu.
-     * On peut l'utiliser pour récupérer des données depuis le modèle
-     * et les affecter à la vue.
-     */
-    protected function beforeRender() {
-
-    }
 }
