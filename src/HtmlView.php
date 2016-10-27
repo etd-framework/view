633c479046f91e37184faaaec5fc72d039b36ec6
Maxence Delattre <maxencedelattre@live.fr>
2016-10-27 14:02:59 +0200

    MAJ Twig Renderer

--- src/HtmlView.php
+++ src/HtmlView.php
@@ -2,7 +2,7 @@
 /**
  * Part of the ETD Framework View Package
  *
- * @copyright   Copyright (C) 2015 ETD Solutions, SARL Etudoo. Tous droits réservés.
+ * @copyright   Copyright (C) 2016 ETD Solutions, SARL Etudoo. Tous droits réservés.
  * @license     Apache License 2.0; see LICENSE
  * @author      ETD Solutions http://etd-solutions.com
  */
@@ -19,5 +19,17 @@ use Joomla\View\BaseHtmlView;
 class HtmlView extends BaseHtmlView implements ContainerAwareInterface {
 
     use ContainerAwareTrait;
+
+    /**
+     * Method to render the view.
+     *
+     * @return  string  The rendered view.
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public function render()
+    {
+        return $this->getRenderer()->render($this->getLayout() . $this->getContainer()->get('config')->get('template.extension'), $this->getData());
+    }
 
 }
